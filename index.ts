e16ab138f99a599b8bfc5abf4726ea262b5e42ba
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-05-24 21:46:15 +0100

    y

--- index.ts
+++ index.ts
@@ -3,34 +3,34 @@ import { cors } from "@elysiajs/cors";
 
 const xSetHeaders = "X-Set-Headers";
 
-export default new Elysia({ aot: false })
-	.onAfterHandle(({ set }) => {
-		set.headers["Access-Control-Allow-Credentials"] = "true";
-	})
-	.use(
-		cors({
-			allowedHeaders: [xSetHeaders, "Access-Control-Allow-Credentials"],
-			origin: "xpui.app.spotify.com",
-		}),
-	)
-	.get("/", () => new Response(undefined, { status: 418 }))
-	.get("/ping/", () => new Response("pong", { status: 200 })) // TODO: can be used to track launches
-	.get("/protocol/*", async context => {
-		const { url } = context.request
-		const strippedPath = url.pathname.slice("/protocol/".length) + url.search + url.hash;
-		const html = `
+export default new Elysia( { aot: false } )
+   .onAfterHandle( ( { set } ) => {
+      set.headers[ "Access-Control-Allow-Credentials" ] = "true";
+   } )
+   .use(
+      cors( {
+         allowedHeaders: [ xSetHeaders, "Access-Control-Allow-Credentials" ],
+         origin: "xpui.app.spotify.com",
+      } ),
+   )
+   .get( "/", () => new Response( undefined, { status: 418 } ) )
+   .get( "/ping/", () => new Response( "pong", { status: 200 } ) ) // TODO: can be used to track launches
+   .get( "/protocol/*", async context => {
+      const url = new URL( context.request.url );
+      const strippedPath = url.pathname.slice( "/protocol/".length ) + url.search + url.hash;
+      const html = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>bespoke protocol</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>bespoke protocol</title>
 </head>
 <body>
-	<script>open("${strippedPath}")</script>
+   <script>open("${ strippedPath }")</script>
 </body>
 </html>
 `;
-		return new Blob([html], { type: "text/html" });
-	});
+      return new Blob( [ html ], { type: "text/html" } );
+   } );
 // .listen(8787);
