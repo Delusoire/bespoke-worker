c66467ac7830cf9578fb3557b5a08edd975e722a
Delusoire <deluso7re@outlook.com>
2024-08-10 02:41:19 +0100

    y

--- index.ts
+++ index.ts
@@ -1,24 +1,23 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 
-const xSetHeaders = "X-Set-Headers";
-
-export default new Elysia( { aot: false } )
-   .onAfterHandle( ( { set } ) => {
-      set.headers[ "Access-Control-Allow-Credentials" ] = "true";
-   } )
-   .use(
-      cors( {
-         allowedHeaders: [ xSetHeaders, "Access-Control-Allow-Credentials" ],
-         origin: "xpui.app.spotify.com",
-      } ),
-   )
-   .get( "/", () => new Response( undefined, { status: 418 } ) )
-   .get( "/ping/", () => new Response( "pong", { status: 200 } ) ) // TODO: can be used to track launches
-   .get( "/protocol/*", async context => {
-      const url = new URL( context.request.url );
-      const strippedPath = url.pathname.slice( "/protocol/".length ) + url.search + url.hash;
-      const html = `
+export default new Elysia({ aot: false })
+	.onAfterHandle(({ set }) => {
+		set.headers["Access-Control-Allow-Credentials"] = "true";
+	})
+	.use(
+		cors({
+			allowedHeaders: ["Access-Control-Allow-Credentials"],
+			origin: "xpui.app.spotify.com",
+		}),
+	)
+	.get("/", () => new Response(undefined, { status: 418 }))
+	.get("/ping/", () => new Response("pong", { status: 200 })) // TODO: can be used to track launches
+	.get("/protocol/*", async (context) => {
+		const url = new URL(context.request.url);
+		const strippedPath = url.pathname.slice("/protocol/".length) +
+			url.search + url.hash;
+		const html = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,10 +26,10 @@ export default new Elysia( { aot: false } )
    <title>bespoke protocol</title>
 </head>
 <body>
-   <script>open("${ strippedPath }")</script>
+   <script>open("${strippedPath}")</script>
 </body>
 </html>
 `;
-      return new Blob( [ html ], { type: "text/html" } );
-   } );
+		return new Blob([html], { type: "text/html" });
+	});
 // .listen(8787);
